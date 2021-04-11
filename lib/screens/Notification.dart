4e458f472a8b9b596e8f27a05a530615aa55fbcf
Amutha varsni R <amuthavarsni.cs18@bitsathy.ac.in>
2021-04-11 15:48:46 +0530

    Notification done

--- lib/screens/Notification.dart
+++ lib/screens/Notification.dart
@@ -16,14 +16,7 @@ class _NotificationPageState extends State<NotificationPage> {
     notificationPlugin.setOnNotificationClick(onNotificationClick);
   }
 
-  final dbRef = FirebaseDatabase.instance
-      .reference()
-      .child("Main Station 1")
-      .orderByKey()
-      .limitToLast(1);
   List<Map<dynamic, dynamic>> lists = [];
-  List<Map<dynamic, dynamic>> lists1 = [];
-  List<Map<dynamic, dynamic>> lists2 = [];
 
   @override
   Widget build(BuildContext context) {
@@ -38,198 +31,14 @@ class _NotificationPageState extends State<NotificationPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            FutureBuilder(
-                future: FirebaseDatabase.instance
-                    .reference()
-                    .child("Main Station 1")
-                    .orderByKey()
-                    .limitToLast(1)
-                    .once(),
-                builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
-                  if (snapshot.hasData) {
-                    lists.clear();
-                    Map<dynamic, dynamic> values = snapshot.data.value;
-                    values.forEach((key, values) {
-                      lists.add(values);
-                    });
-                    print(lists[0]["Quality"]);
-                    if (!(lists[0]["Quality"] > 6.5 &&
-                        lists[0]["Quality"] < 8.5)) {
-                      notificationPlugin.showNotification("Test", "Test");
-                    }
-                    if (lists[0]["Flowrate"] == 0) {
-                      if (lists[0]["Limit"] != lists[0]["WaterRecieved"]) {
-                        notificationPlugin.showNotification(
-                            "There is a leakage", "Pipes in Main Station 1");
-                      }
-                    }
-                    return new ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: lists.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    "Main Station 1",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize:
-                                          MediaQuery.of(context).size.width *
-                                              0.04,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text("Flowrate: " +
-                                      lists[index]["Flowrate"].toString()),
-                                  Text("Quality: " +
-                                      lists[index]["Quality"].toString()),
-                                  Text("WaterRecieved: " +
-                                      lists[index]["WaterRecieved"].toString()),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }),
-            FutureBuilder(
-                future: FirebaseDatabase.instance
-                    .reference()
-                    .child("Sub Station 1")
-                    .orderByKey()
-                    .limitToLast(1)
-                    .once(),
-                builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
-                  if (snapshot.hasData) {
-                    lists1.clear();
-                    Map<dynamic, dynamic> values = snapshot.data.value;
-                    values.forEach((key, values) {
-                      lists1.add(values);
-                    });
-                    print(lists1[0]["Quality"]);
-                    if (!(lists1[0]["Quality"] > 6.5 &&
-                        lists1[0]["Quality"] < 8.5)) {
-                      notificationPlugin.showNotification("Water Quality is bad", "In Sub Station 1");
-                    }
-                    if (lists1[0]["Flowrate"] == 0) {
-                      if (lists1[0]["Limit"] != lists1[0]["WaterRecieved"]) {
-                        notificationPlugin.showNotification(
-                            "There is a leakage", "Pipes in Main Station 1");
-                      }
-                    }
-                    return new ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: lists.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    "Sub Station 1",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize:
-                                      MediaQuery.of(context).size.width *
-                                          0.04,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text("Flowrate: " +
-                                      lists1[index]["Flowrate"].toString()),
-                                  Text("Quality: " +
-                                      lists1[index]["Quality"].toString()),
-                                  Text("WaterRecieved: " +
-                                      lists1[index]["WaterRecieved"].toString()),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }),
-            FutureBuilder(
-                future: FirebaseDatabase.instance
-                    .reference()
-                    .child("Sub Station 2")
-                    .orderByKey()
-                    .limitToLast(1)
-                    .once(),
-                builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
-                  if (snapshot.hasData) {
-                    lists2.clear();
-                    Map<dynamic, dynamic> values = snapshot.data.value;
-                    values.forEach((key, values) {
-                      lists2.add(values);
-                    });
-                    print(lists2[0]["Quality"]);
-                    if (!(lists2[0]["Quality"] > 6.5 &&
-                        lists2[0]["Quality"] < 8.5)) {
-                      notificationPlugin.showNotification("Water Quality is bad", "In Sub Station 1");
-                    }
-                    if (lists2[0]["Flowrate"] == 0) {
-                      if (lists2[0]["Limit"] != lists2[0]["WaterRecieved"]) {
-                        notificationPlugin.showNotification(
-                            "There is a leakage", "Pipes in Main Station 1");
-                      }
-                    }
-                    return new ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: lists2.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    "Sub Station 2",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize:
-                                      MediaQuery.of(context).size.width *
-                                          0.04,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text("Flowrate: " +
-                                      lists2[index]["Flowrate"].toString()),
-                                  Text("Quality: " +
-                                      lists2[index]["Quality"].toString()),
-                                  Text("WaterRecieved: " +
-                                      lists2[index]["WaterRecieved"].toString()),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                })
+            Stationcheck("Main Station 1"),
+            Stationcheck("Sub Station 1"),
+            Stationcheck("Sub Station 2"),
           ],
         ),
       ),
     );
   }
-
   onNotificationInLowerVersions(ReceivedNotification receivedNotification) {
     print('Notification Received ${receivedNotification.id}');
   }
@@ -238,6 +47,82 @@ class _NotificationPageState extends State<NotificationPage> {
     print('Payload $payload');
   }
 }
+
+class Stationcheck extends StatelessWidget {
+  @override
+  String station;
+  List<Map<dynamic, dynamic>> lists = [];
+  Stationcheck(this.station);
+
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: FirebaseDatabase.instance
+            .reference()
+            .child(station)
+            .orderByKey()
+            .limitToLast(1)
+            .once(),
+        builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+          if (snapshot.hasData) {
+            lists.clear();
+            Map<dynamic, dynamic> values = snapshot.data.value;
+            values.forEach((key, values) {
+              lists.add(values);
+            });
+            print(lists[0]["Quality"]);
+            if (!(lists[0]["Quality"] > 6.5 &&
+                lists[0]["Quality"] < 8.5)) {
+              notificationPlugin.showNotification("Test", "Test");
+            }
+            if (lists[0]["Flowrate"] == 0) {
+              if (lists[0]["Limit"] != lists[0]["WaterRecieved"]) {
+                notificationPlugin.showNotification(
+                    "There is a leakage", "Pipes in Main Station 1");
+              }
+            }
+            return new ListView.builder(
+                shrinkWrap: true,
+                itemCount: lists.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Text(
+                            station,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize:
+                              MediaQuery.of(context).size.width *
+                                  0.04,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text("Flowrate: " +
+                              lists[index]["Flowrate"].toString()),
+                          Text("Quality: " +
+                              lists[index]["Quality"].toString()),
+                          Text("WaterRecieved: " +
+                              lists[index]["WaterRecieved"].toString()),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+}
+
+
+
+
 
 class NotificationWidget extends StatelessWidget {
   @override
